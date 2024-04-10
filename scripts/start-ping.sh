bb21aa7c05a040302e89cecf2a364e6aa9852dc2
Christoph Hummer <christophhummer@gmail.com>
2024-04-10 19:45:29 +0200

    scripts/start-ping.sh aktualisiert

--- scripts/start-ping.sh
+++ scripts/start-ping.sh
@@ -3,7 +3,7 @@ sleep 10
 while true; do
   if ! ping -c 1 -W 10 ${PING_IP} >/dev/null 2>&1 ; then
     echo "---Ping from ${PING_IP} failed, restarting container,...---"
-    kill -SIGINT $(pidof openvpn)
+    kill 1
   fi
   sleep ${PING_INTERVAL}s
 done
