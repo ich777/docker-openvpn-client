8631dd37b0c4c45c5f361c3fbfbab2ab53f44bc7
Christoph Hummer <christophhummer@gmail.com>
2024-04-16 18:23:48 +0200

    scripts/start-ping.sh aktualisiert

--- scripts/start-ping.sh
+++ scripts/start-ping.sh
@@ -3,7 +3,7 @@ sleep 10
 while true; do
   if ! ping -c 1 -W 10 ${PING_IP} >/dev/null 2>&1 ; then
     echo "---Ping from ${PING_IP} failed, restarting container,...---"
-    kill 1
+    kill -SIGTERM 1
   fi
   sleep ${PING_INTERVAL}s
 done
