1b27d754920c16b160e0e427f240279689972103
Christoph Hummer <christophhummer@gmail.com>
2024-07-21 08:35:01 +0200

    connected-containers_scripts/connected-containers-alpine.sh aktualisiert

--- connected-containers_scripts/connected-containers-alpine.sh
+++ connected-containers_scripts/connected-containers-alpine.sh
@@ -2,4 +2,4 @@
 apk update
 apk add netcat-openbsd
 echo "---Starting connected containers watchdog on 127.0.0.1:27286---"
-(nc 127.0.0.1 27286 && { echo "---Connection to connected container lost, restarting in 15 seconds...---"; sleep 15; kill -SIGTERM 1; } || echo "---Couldn't connect to connected containers---") &
+(sleep 10 && nc 127.0.0.1 27286 && { echo "---Connection to connected container lost, restarting in 15 seconds...---"; sleep 15; kill -SIGTERM 1; } || echo "---Couldn't connect to connected containers---") &
